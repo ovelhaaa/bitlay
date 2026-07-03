1ade0942a6f3d5e22732b40fd2a9255ecd687041
ovelhaaa <ovelhaaa@gmail.com>
2026-07-03 16:17:38 -0300

    Fase 1: Limpeza de artefatos mortos e dependencias não utilizadas

--- fix_edu.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/App.tsx', 'utf8');
-
-content = content.replace(/\s*{\/\* 4\. Educational Details Box \*\//, `\n                  </div>\n                </div>\n                {/* 4. Educational Details Box */}`);
-
-fs.writeFileSync('src/App.tsx', content);
--- fix_faceplate.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/App.tsx', 'utf8');
-
-content = content.replace(/                      <\/button>\s*{\/\* Group 1:/g, `                      </button>\n                    </div>\n                  </div>\n                  {/* Group 1:`);
-
-fs.writeFileSync('src/App.tsx', content);
